311fdb7127c06ceee7fd5277861eb7cd9851af86
Jason Dichoso <jpsdichoso21@gmail.com>
2024-07-09 09:47:30 +0800

    feat: mongodb connection

--- backend/main.go
+++ backend/main.go
@@ -1,30 +1,59 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+    "log"
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+    "net/http"
 )
 
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
+type todoItem struct {
+    ID int `json:"id" bson:"_id"`
+    Completed bool `json:"completed?"`
+    Body string `json:"body"`
 }
 
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+var collection *mongo.Collection
 
 func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
+    router := gin.Default()
 
-	router.Run("localhost:8080")
-}
+    router.SetTrustedProxies(nil)
 
-func getAlbums(ctx *gin.Context) {
-    ctx.IndentedJSON(http.StatusOK, albums)
+    router.GET("/", func(ctx *gin.Context) {
+        ctx.IndentedJSON(http.StatusOK, gin.H{
+            "message": "Server is running on port 8080",
+        })
+    })
+    
+    err := godotenv.Load(".env")
+
+    if err != nil {
+        log.Fatal("Error loading .env: ", err)
+    }
+
+    MONGODB_URI := os.Getenv("MONGODB_URI")
+    clientOptions := options.Client().ApplyURI(MONGODB_URI)
+    client, err := mongo.Connect(context.Background(), clientOptions)
+
+    if err != nil {
+       log.Fatal("Mongo connection error: ", err)
+    }
+
+    err = client.Ping(context.Background(), nil)
+
+    if err != nil {
+        log.Fatal("Mongo connection error: ", err)
+    }
+
+    fmt.Println("Established connection to MongoDB Atlas.")
+
+    collection = client.Database("golang_db").Collection("todos")
+
+    router.Run("localhost:8080")
 }
